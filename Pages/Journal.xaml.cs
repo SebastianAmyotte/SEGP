780bf56b25eb773585de82cf8b567af75dd8fe78
Sebastian Amyotte <71189225+SebastianAmyotte@users.noreply.github.com>
2022-11-07 10:25:40 -0600

    Updates to Journal

--- Pages/Journal.xaml.cs
+++ Pages/Journal.xaml.cs
@@ -2,26 +2,83 @@ namespace SEGP.Pages;
 
 public partial class Journal : ContentPage
 {
-    List<JournalEntry> dailyEntries;
+    Dictionary<DateTime, JournalEntry> dailyEntries;
+    DateTime currentDay = DateTime.Today;
+    JournalEntry currentDisplayedEntry;
 
 	public Journal()
 	{
         InitializeComponent();
+        //TODO: Serialize dailyEntries, for now create blank entry
+        dailyEntries = new Dictionary<DateTime, JournalEntry>();
+        updateCurrentEntry();
     }
 
 	void OnBackButtonPressed(object sender, EventArgs e)
 	{
-        //Change the page a day back
-        DisplayAlert("Error", "No previous day found", "OK");
+        currentDay = currentDay.AddDays(-1);
+        if (changesMade())
+        {
+            Task<bool> userChoice = DisplayAlert("Changes made", "Unsaved changes, would you like to save?", "Yes", "No");
+            if (userChoice.Result)
+            {
+                OnSaveButtonPressed(null, null);
+            }
+        }
+        updateCurrentEntry();
 	}
     void OnNextButtonPressed(object sender, EventArgs e)
     {
-        //Change the page a day forward
-        DisplayAlert("Error", "No next day found", "OK");
+        currentDay = currentDay.AddDays(1);
+        if (currentDay > DateTime.Now)
+        {
+            DisplayAlert("Error", "Tomorrow hasn't started yet!", "OK");
+            currentDay = currentDay.AddDays(-1);
+        } else
+        {
+            if (changesMade())
+            {
+                Task<bool> userChoice = DisplayAlert("Changes made", "Unsaved changes, would you like to save?", "Yes", "No");
+                if (userChoice.Result)
+                {
+                    OnSaveButtonPressed(null, null);
+                }
+            }
+            updateCurrentEntry();
+        }
+        
     }
+
+    void updateCurrentEntry()
+    {
+        //Update data structure
+        if (dailyEntries.ContainsKey(currentDay))
+        {
+            currentDisplayedEntry = dailyEntries[currentDay];
+        }
+        else
+        {
+            currentDisplayedEntry = new JournalEntry("", "", currentDay);
+            dailyEntries.Add(currentDay, currentDisplayedEntry);
+        }
+        //Update UI
+        date.Text = currentDay.ToString();
+        rating.Text = currentDisplayedEntry.GetRating();
+        dailyThoughts.Text = currentDisplayedEntry.GetEntry();
+    }
+
+    bool changesMade()
+    {
+        bool changesMadeToRating = currentDisplayedEntry.GetRating().Equals(rating.Text);
+        bool changesMadeToJournalEntry = currentDisplayedEntry.GetEntry().Equals(dailyThoughts.Text);
+        return changesMadeToRating || changesMadeToJournalEntry;
+    }
+
 
     void OnSaveButtonPressed(object sender, EventArgs e)
     {
+        currentDisplayedEntry.SaveEntry(dailyThoughts.Text);
+        currentDisplayedEntry.SaveRating(rating.Text);
         DisplayAlert("Saved", "Journal entry saved", "OK");
     }
 
@@ -40,22 +97,31 @@ public class JournalEntry
 {
     String entry;
     String rating;
-    String date;
+    DateTime date;
 
-    JournalEntry(String entry, String rating, string date)
+    public JournalEntry(String entry, String rating, DateTime date)
     {
         this.entry = entry;
         this.rating = rating;
         this.date = date;
     }
 
-    String GetEntry()
+    public String GetEntry()
     {
         return entry;
     }
 
-    String GetRating()
+    public void SaveEntry(String entry)
+    {
+        this.entry = entry;
+    }
+    public String GetRating()
     {
         return rating;
     }
+    public void SaveRating(String rating)
+    {
+        this.rating = rating;
+    }
+
 }
